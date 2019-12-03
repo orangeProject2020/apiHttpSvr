59263226fc4c7028e4d52717568b122a0c0b34ed
lucong <332553882@qq.com>
2019-12-03 17:24:38 +0800

    update

--- notify.js
+++ notify.js
@@ -7,15 +7,15 @@ const mallClient = clients['mallProxyClient']
 const log = require('./lib/log')('notify')
 
 router.post('/alipay', async function (req, res) {
-  log.info('/alipay body:' , body)
+  log.info('/alipay body:', body)
   let data = {
     ...req.body
   }
   data.uuid = uuid.v4()
-  ret = await mallClient.request('notify_alipay', data)
+  ret = await mallClient.request('order_notifyAlipay', data)
 
   console.log('/alipay ret:', ret)
-  if (ret.code === '0'){
+  if (ret.code === '0') {
     res.send('success')
   } else {
     res.send('fail')
